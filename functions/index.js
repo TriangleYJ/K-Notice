929c59133557d6c815eb8a4c40d919b7b4664f2a
Triangleyj <dpwns99@gmail.com>
2021-01-01 15:36:27 +0900

    First listener commit

--- functions/index.js
+++ functions/index.js
@@ -38,91 +38,6 @@ const getDateStringBefore = days => {
 const fds = str => str.replace(/\./g, "-")
 const sdf = str => str.replace(/-/g, ".")
 
-
-/*eslint-disable */
-const parse = async date_string => {
-    try {
-        const browser = await puppeteer.launch({args: [ '--no-sandbox', '--disable-setuid-sandbox']})
-        const page = await browser.newPage()
-
-        await page.goto('https://portal.kaist.ac.kr')
-        await page.evaluate((id, pw) => {
-            document.querySelector('input[name="userId"]').value = id;
-            document.querySelector('input[name="password"]').value = pw;
-        }, my_id, my_pw);
-        await page.click('a[name="btn_login"]');
-
-        await page.waitForNavigation()
-
-        const parser_until_date = async date => {
-            let page_num = 1;
-            let list = [];
-            while (page_num <= MAX_PAGE_COUNT) {
-                await page.goto(`https://portal.kaist.ac.kr/board/list.brd?boardId=today_notice&lang_knd=ko&userAgent=Chrome&isMobile=false&page=${page_num.toString()}&userAgent=Chrome&isMobile=False&sortColumn=REG_DATIM&sortMethod=DESC`)
-                let rows = await page.$$("table > tbody > tr");
-                rows.shift();
-                for (let row of rows) {
-                    let new_row = []
-                    const cols = await row.$$("td")
-                    for (let col_num in cols) {
-                        let text = await page.evaluate(element => element.textContent, cols[col_num]);
-                        text = text.replace(/\t/g, '').replace(/\n/g, '')
-                        if (col_num === "3") text = parseInt(text)
-                        new_row.push(text)
-                        if (col_num === "4" && date.getTime() > new Date(fds(text)).getTime()){
-                            await browser.close()
-                            return list
-                        }
-                    }
-                    let href = await page.evaluate(element => element.getAttribute('href'), await cols[0].$("a"))
-                    new_row.push(href)
-                    list.push(new_row)
-                }
-                page_num++
-            }
-            await browser.close()
-            return list
-        }
-
-
-        return await parser_until_date(new Date(date_string))
-
-    } catch (e) {
-        functions.logger.error("Crawling Failed! : " + e.toString(), {structuredData: true});
-        console.error("Crawling Failed! : " + e.toString(), {structuredData: true});
-    }
-}
-
-const daily_updater = async () => {
-    const DB_1days = await parse(getDateStringBefore(1))
-    const ref = await defaultDatabase.ref('notices')
-    for(let i of DB_1days) {
-        await ref.orderByChild("href").equalTo(i[5]).once("value", snapshot => {
-            if (snapshot.exists()) {
-                const my_key = Object.keys(snapshot.val())[0];
-                ref.child(`${my_key}/title`).set(i[0])
-                ref.child(`${my_key}/views/${new Date().getTime()}`).set(i[3])
-            } else {
-                ref.push({
-                    title: i[0],
-                    belong: i[1],
-                    writer: i[2],
-                    views: {
-                        [new Date().getTime()]: i[3]
-                    },
-                    date: i[4],
-                    href: i[5]
-                })
-            }
-        });
-    }
-
-
-    return null;
-}
-/*eslint-enable */
-
-
 const top_notice = async (st, ed, days) => {
     let db = {}
 
@@ -151,14 +66,6 @@ const top_notice = async (st, ed, days) => {
     return stringBuilder
 }
 
-
-const main = async () => {
-    await bot.sendMessage(MY_CHAT_ID, "하루 동안 가장 인기있었던 공지입니다.")
-    await bot.sendMessage(MY_CHAT_ID, await top_notice(1, 10, 1), {parse_mode: "HTML"})
-    return null;
-}
-
-
 bot.onText(/\/next(.+)_(.+)_(.+)/, async (msg, match) => {
     await bot.sendMessage(MY_CHAT_ID, await top_notice(parseInt(match[2]), parseInt(match[3]),parseInt(match[1])), {parse_mode: "HTML"});
 });
@@ -168,55 +75,3 @@ bot.onText(/n (.+)/g, async (msg, match) => {
     bot.sendMessage(MY_CHAT_ID, `${match[1]}일 동안 가장 인기있었던 공지입니다.`)
     bot.sendMessage(MY_CHAT_ID, await top_notice(1, 10, parseInt(match[1])), {parse_mode: "HTML"})
 })
-
-
-app.get('/hello', async (req, res) => {
-    const ref = await defaultDatabase.ref('notices')
-    let dummy = {}
-    await ref.orderByChild('date').startAt('2020.12.25').once("value", snapshot => {
-        dummy = snapshot.val()
-    })
-    await res.send()
-})
-
-app.listen(3000, () => {
-    console.log("Start to listen at 3000!")
-});
-
-exports.api = functions.https.onRequest(app)
-
-const runtimeOpts = {
-    timeoutSeconds: 90,
-    memory: '1GB'
-}
-
-
-
-exports.notice_updater = functions.region('asia-northeast1').runWith(runtimeOpts).pubsub.schedule('*/10 * * * *').onRun(async (context) => {
-    return daily_updater()
-});
-
-exports.notice_alert = functions.region('asia-northeast1').pubsub.schedule('0 9,18 * * *').onRun(async (context) => {
-    return main()
-});
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-
-
-/*
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
-});
-
-exports.mysite = functions.https.onRequest((req, res) => {
-  res.send("Wow");
-});
-*/
-
-/*
-firebase deploy
-firebase emulators:start
- */
